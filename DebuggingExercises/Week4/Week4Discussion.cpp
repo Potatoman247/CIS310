d86b4190a23d48e9f6907d75069194994f4b6048
Aidan Jacoby <109009247+Potatoman247@users.noreply.github.com>
2024-09-18 15:31:56 -0700

    Create Week4Discussion.cpp

---
+++ DebuggingExercises/Week4/Week4Discussion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    while false {
+      cout >> 'Hello World!';
+    }
+    do 
+      cout >> 'Goodbye World!'
+    while true;
+  
+    return 0;
+}
