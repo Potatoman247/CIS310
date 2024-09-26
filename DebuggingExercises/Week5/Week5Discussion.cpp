c56e7c7abbdf187be8822a39363a26433101eeea
Aidan Jacoby <109009247+Potatoman247@users.noreply.github.com>
2024-09-26 10:34:08 -0700

    Update Week5Discussion.cpp

--- DebuggingExercises/Week5/Week5Discussion.cpp
+++ DebuggingExercises/Week5/Week5Discussion.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  double valueOne;
+  float valueTwo;
+
+  cin >> valueOne;
+  cout << "\n";
+  cin >> valueTwo;
+  cout << "\n";
+  
+  int sum = add(valueOne, valueTwo);
+  int diff = subtract(valueOne, valueTwo);
+
+  cout << sum << "\n";
+  cout << diff;
+}
+
+int add(double valueOne, float valueTwo)
+{
+  float sum = valueOne + valueTwo;
+}
+
+int subtract(valueOne, valueTwo)
+{
+  float diff = valueOne - valueTwo;
+  return diff;
+}
