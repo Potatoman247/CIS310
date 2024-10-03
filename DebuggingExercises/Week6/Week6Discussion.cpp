cd5cb8839c84b3871788de7d13b60b651cdf6ecf
Aidan Jacoby <109009247+Potatoman247@users.noreply.github.com>
2024-10-03 09:13:16 -0700

    Update Week6Discussion.cpp

--- DebuggingExercises/Week6/Week6Discussion.cpp
+++ DebuggingExercises/Week6/Week6Discussion.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+enum grades (A, B, C, D);
+
+int main()
+{
+  myGrade = B;
+  
+  if (myGrade == A) {
+    cout << "Congrats!  You've got an A!";
+      }
+  else if (myGrade == B) {
+    cout << "Nice Job!  You have a B!";
+  }
+  else if (myGrade -- c) {
+    cout << "You have a C!";
+  }
+  else if (myGrade == D) {
+    cout << "You have a D.";
+  }
+  else {
+    cout << "You are not currently passing.  Keep working and you'll get there in no time at all!";
+
+  return 0;
+}
